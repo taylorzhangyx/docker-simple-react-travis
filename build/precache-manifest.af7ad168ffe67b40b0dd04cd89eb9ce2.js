858c5cfef22b09d368daa604081c7a7b9669f42c
taylorzhangyx <taylorzhangyx@yahoo.com>
2019-09-15 14:12:08 -0700

    copy all react here

---
+++ build/precache-manifest.af7ad168ffe67b40b0dd04cd89eb9ce2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c99512dab2cb9695bbde95e3916c08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85f8c0d8aa6a75b19708",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "bbf01b64404ab1d76e97",
+    "url": "/static/js/2.3ef24090.chunk.js"
+  },
+  {
+    "revision": "85f8c0d8aa6a75b19708",
+    "url": "/static/js/main.4a0f5710.chunk.js"
+  },
+  {
+    "revision": "e3286b98aa671f6c4418",
+    "url": "/static/js/runtime~main.a36b6f19.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
